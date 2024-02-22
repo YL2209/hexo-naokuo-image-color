a53ffab7ca82479494e1bc613dc30741b19554cf
YL2209 <3021027861@qq.com>
2024-02-22 22:13:20 +0800

    npm publish

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ npm i hexo-naokuo-image-color --save
 
 ### 用法
 
-
-
+- [anzhiyu主题](https://github.com/anzhiyu-c/hexo-theme-anzhiyu) 安装后打开对应配置直接使用
+ 
 ### 引用库
 
 - [color-thief-node](https://github.com/zicodeng/color-thief-node)
 - [@napi-rs/canvas](https://github.com/Brooooooklyn/canvas)
+- 部分代码来自[hexo-theme-anzhiyu](https://github.com/anzhiyu-c/hexo-theme-anzhiyu)
--- index.js
+++ index.js
@@ -1,33 +1,95 @@
 'use strict';
 
-const { getColorFromURL} = require('./src/color-thief');
+const { getColorFromURL } = require('./src/color-thief');
 
 hexo.extend.filter.register('before_post_render', async function (data) {
   const { cover } = data;
 
-  if (cover) {
-    try {
+  if (!cover) return;
 
-      // 获取文章封面图片路径
-      const imageUrl = cover;
+  try {
+    // 获取文章封面图片路径
+    const imageUrl = cover;
+    // 异步获取图片主色调
+    const dominantColor = await getColorFromURL(imageUrl);
+    // 将颜色转换为HEX格式
+    const ImgColorHex = colorHex(`rgb(${dominantColor})`);
+    let adjustedColor = ImgColorHex;
 
-      // 获取图片主色调
-      const dominantColor = await getColorFromURL(imageUrl);
-
-      // 将颜色转换为HEX格式
-      const hexColor = rgbToHex(dominantColor[0], dominantColor[1], dominantColor[2]);
-
-      // 将主题色添加到文章数据中
-      // console.log(hexColor);
-      data.main_color = hexColor;
-    } catch (error) {
-      console.error(`Error fetching theme color from image: ${error}`);
+    // 图片亮度调整
+    if (getContrastYIQ(ImgColorHex) === "light") {
+      adjustedColor = LightenDarkenColor(ImgColorHex, -40);
     }
+    // 将主题色添加到文章数据中
+    // console.log(adjustedColor);
+    data.main_color = adjustedColor;
+  } catch (error) {
+    console.error(`从图像中提取主题颜色时出错: ${error}`);
   }
 
   return data;
 });
 
-function rgbToHex(r, g, b) {
-  return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-}
+//RGB颜色转化为16进制颜色
+const colorHex = str => {
+  const hexRegex = /^#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/;
+
+  if (/^(rgb|RGB)/.test(str)) {
+    const aColor = str.replace(/(?:\(|\)|rgb|RGB)*/g, "").split(",");
+    return aColor.reduce((acc, val) => {
+      const hex = Number(val).toString(16).padStart(2, "0");
+      return acc + hex;
+    }, "#");
+  }
+
+  if (hexRegex.test(str)) {
+    if (str.length === 4) {
+      return Array.from(str.slice(1)).reduce((acc, val) => acc + val + val, "#");
+    }
+    return str;
+  }
+
+  return str;
+};
+
+// Determine whether a color is light or dark
+const getContrastYIQ = hexcolor => {
+  const colorRgb = color => {
+    const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+    color = color.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
+    return result ? `rgb(${parseInt(result[1], 16)}, ${parseInt(result[2], 16)}, ${parseInt(result[3], 16)})` : null;
+  };
+
+  const colorrgb = colorRgb(hexcolor);
+  const colors = colorrgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+
+  const [_, red, green, blue] = colors;
+
+  const brightness = (red * 299 + green * 587 + blue * 114) / 255000;
+
+  return brightness >= 0.5 ? "light" : "dark";
+};
+
+// Lighten or darken a color
+const LightenDarkenColor = (col, amt) => {
+  const usePound = col.startsWith("#");
+
+  if (usePound) {
+    col = col.slice(1);
+  }
+
+  let num = parseInt(col, 16);
+
+  const processColor = (colorValue, amount) => {
+    colorValue += amount;
+    return colorValue > 255 ? 255 : colorValue < 0 ? 0 : colorValue;
+  };
+
+  const r = processColor(num >> 16, amt);
+  const b = processColor((num >> 8) & 0x00ff, amt);
+  const g = processColor(num & 0x0000ff, amt);
+
+  return (usePound ? "#" : "") + String("000000" + (g | (b << 8) | (r << 16)).toString(16)).slice(-6);
+};
